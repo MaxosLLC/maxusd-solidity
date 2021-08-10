39976c081bdcae06ec162ecf307f445b051dc4a1
devneser <zh_alex@yahoo.com>
2021-08-10 19:51:21 +0400

    refactor: update YearnUSDCStrategy with Chainlink

--- contracts/strategies/YearnUSDCStrategy.sol
+++ contracts/strategies/YearnUSDCStrategy.sol
@@ -6,41 +6,41 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+import "../interfaces/IStrategyBase.sol";
+import "../interfaces/IStrategyAssetValue.sol";
 import "../interfaces/IYearnUSDCVault.sol";
-import "./Collateral.sol";
 
-contract CollateralSingleYearnVaultStrategy is Ownable, Pausable, ReentrancyGuard {
+contract YearnUSDCStrategy is IStrategyBase, IStrategyAssetValue, Ownable, Pausable, ReentrancyGuard {
   using SafeERC20 for IERC20;
 
   IYearnUSDCVault public yVault;
-  Collateral public collateral;
+  IERC20 public baseToken;
+  AggregatorV3Interface internal priceFeed;
 
-  uint256 public totalShares = 0;
+  uint256 public totalDeposits = 0;
 
-  modifier onlyCollateral() {
-    require(msg.sender == address(collateral), "CSYVS: caller should be collateral");
-    _;
-  }
+  address public constant CHAINLINK_USDC_USD = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
 
-  constructor(address _vault, address _collateral) {
-    require(
-      IYearnUSDCVault(_vault).token() == address(Collateral(_collateral).baseToken()),
-      "CSYVS: vault and collateral are not matched"
-    );
+  constructor(address _vault, address _token) {
+    require(IYearnUSDCVault(_vault).token() == _token, "YUS: vault and base token are not matched");
     yVault = IYearnUSDCVault(_vault);
-    collateral = Collateral(_collateral);
-    totalShares = 0;
+    baseToken = IERC20(_token);
+    priceFeed = AggregatorV3Interface(CHAINLINK_USDC_USD);
+    totalDeposits = 0;
   }
 
-  function deposit(uint256 _amount) external nonReentrant whenNotPaused returns (uint256) {
-    uint256 deposited = _deposit(_amount);
-    return deposited;
+  function invest(uint256 amount) external override nonReentrant whenNotPaused {
+    _deposit(amount);
   }
 
-  function withdraw(uint256 _amount) external onlyCollateral nonReentrant returns (uint256) {
-    uint256 withdrawal = _withdraw(_amount);
-    return withdrawal;
+  function redeem(address beneficiary, uint256 amount) external override nonReentrant whenNotPaused {
+    _withdraw(amount, beneficiary);
+  }
+
+  function strategyAssetValue() external view override returns (uint256) {
+    return (totalDeposits * uint256(_getThePrice())) / 8;
   }
 
   function pause() external {
@@ -51,52 +51,44 @@ contract CollateralSingleYearnVaultStrategy is Ownable, Pausable, ReentrancyGuar
     _unpause();
   }
 
-  function withdrawAll() external onlyOwner {
-    yVault.withdraw(totalShares);
-    totalShares = 0;
-  }
-
   function migrate(address _vault) external onlyOwner {
     require(_vault != address(0), "CSYVS: invalid new vault");
-    require(IYearnUSDCVault(_vault).token() == address(currency()), "CSYVS: vault not matched");
+    require(IYearnUSDCVault(_vault).token() == address(baseToken), "YUS: vault not matched");
 
-    uint256 _before = currency().balanceOf(address(this));
-    yVault.withdraw(totalShares);
-    uint256 _after = currency().balanceOf(address(this));
+    uint256 _before = baseToken.balanceOf(address(this));
+    yVault.withdraw(totalDeposits);
+    uint256 _after = baseToken.balanceOf(address(this));
     yVault = IYearnUSDCVault(_vault);
     yVault.deposit(_after - _before);
   }
 
-  function totalValue() external view returns (uint256) {
-    // TODO Need to declare total value
-  }
-
-  function currency() public view returns (IERC20) {
-    return collateral.baseToken();
-  }
-
   function _deposit(uint256 _amount) internal returns (uint256) {
-    require(_amount > 0 && _amount <= yVault.availableDepositLimit(), "CSYVS: invalid deposit amount");
-    uint256 _before = currency().balanceOf(address(this));
-    currency().safeTransferFrom(msg.sender, address(this), _amount);
-    uint256 _after = currency().balanceOf(address(this));
+    require(_amount > 0 && _amount <= yVault.availableDepositLimit(), "YUS: invalid deposit amount");
+    uint256 _before = baseToken.balanceOf(address(this));
+    baseToken.safeTransferFrom(msg.sender, address(this), _amount);
+    uint256 _after = baseToken.balanceOf(address(this));
     _amount = _after - _before;
 
     yVault.deposit(_amount);
-    totalShares += _amount;
+    totalDeposits += _amount;
 
     return _amount;
   }
 
-  function _withdraw(uint256 _amount) internal returns (uint256) {
-    require(_amount > 0 && _amount <= yVault.balanceOf(msg.sender), "CSYVS: invalid withdraw amount");
-    uint256 _before = currency().balanceOf(address(this));
+  function _withdraw(uint256 _amount, address _recipient) internal returns (uint256) {
+    require(_amount > 0 && _amount <= yVault.balanceOf(msg.sender), "YUS: invalid withdraw amount");
+    uint256 _before = baseToken.balanceOf(address(this));
     yVault.withdraw(_amount);
-    uint256 _after = currency().balanceOf(address(this));
+    uint256 _after = baseToken.balanceOf(address(this));
     _amount = _after - _before;
-    currency().safeTransfer(address(collateral), _amount);
-    totalShares -= _amount;
+    baseToken.safeTransfer(address(_recipient), _amount);
+    totalDeposits -= _amount;
 
     return _amount;
+  }
+
+  function _getThePrice() internal view returns (int256) {
+    (, int256 price, , , ) = priceFeed.latestRoundData();
+    return price;
   }
 }
