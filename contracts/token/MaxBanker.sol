618ec749627d33a68fade05287b82d1313e21ceb
0xSolcroot <de.solcroot@gmail.com>
2021-08-12 03:24:08 +1000

    refactor MaxBanker contract using ownable and implement transfer control feature

--- contracts/token/MaxBanker.sol
+++ contracts/token/MaxBanker.sol
@@ -1,36 +1,38 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 
 /**
  * @notice MaxBanker Contract
  * @author Maxos
  */
-contract MaxBanker is ERC20Upgradeable, AccessControlUpgradeable, PausableUpgradeable {
+contract MaxBanker is PausableUpgradeable, OwnableUpgradeable, ERC20Upgradeable {
   /*** Events ***/
   event Mint(address indexed to, uint256 amount);
   event Burn(address indexed from, uint256 amount);
+  event TransferFailed(address indexed from, address indexed to, uint256 _amount, uint256 indexed transferControl);
 
   /*** Constants ***/
-  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-  bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
-  bytes32 public constant PAUSABLE_ROLE = keccak256("PAUSABLE_ROLE");
 
   /*** Storage Properties ***/
+  address public mintContract;
+  uint256 public transferControl;
+  mapping(address => bool) public whiteList;
 
   /*** Contract Logic Starts Here */
 
+  modifier onlyMinter() {
+    require(msg.sender == super.owner() || msg.sender == mintContract, "No minter");
+    _;
+  }
+
   function initialize(string memory name, string memory symbol) public initializer {
     __ERC20_init_unchained(name, symbol);
-    __AccessControl_init_unchained();
+    __Ownable_init_unchained();
     __Pausable_init();
-
-    // Grant the contract deployer the default admin role: it will be able
-    // to grant and revoke any roles
-    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
   }
 
   /**
@@ -39,7 +41,7 @@ contract MaxBanker is ERC20Upgradeable, AccessControlUpgradeable, PausableUpgrad
    * @param to address to receive the token minted
    * @param amount token amount to be minted
    */
-  function mint(address to, uint256 amount) external virtual onlyRole(MINTER_ROLE) whenNotPaused {
+  function mint(address to, uint256 amount) external onlyMinter whenNotPaused {
     _mint(to, amount);
 
     emit Mint(to, amount);
@@ -49,38 +51,101 @@ contract MaxBanker is ERC20Upgradeable, AccessControlUpgradeable, PausableUpgrad
    * @notice Burn `amount` tokens from the caller
    * @param amount token amount to burn
    */
-  function burn(uint256 amount) external virtual whenNotPaused {
+  function burn(uint256 amount) external whenNotPaused {
     _burn(msg.sender, amount);
 
     emit Burn(address(this), amount);
   }
 
   /**
-   * @notice Burn `amount` tokens from `account`, deducting from the caller's allowance
-   * @dev caller must have the burner role and have allowance for `from`'s tokens of at least `amount`.
-   * @param from address from which the token will be burned
-   * @param amount token amount to be burned
+   * @notice Pause MaxBaner
    */
-  function burnFrom(address from, uint256 amount) external virtual onlyRole(BURNER_ROLE) whenNotPaused {
-    uint256 currentAllowance = allowance(from, msg.sender);
-    require(currentAllowance >= amount, "PToken: exceeds allowance");
-    _approve(from, msg.sender, currentAllowance - amount);
-    _burn(from, amount);
-
-    emit Burn(from, amount);
-  }
-
-  /**
-   * @notice Pause Maxos
-   */
-  function pause() external onlyRole(PAUSABLE_ROLE) whenNotPaused {
+  function pause() external onlyOwner whenNotPaused {
     super._pause();
   }
 
   /**
-   * @notice Unpause Maxos
+   * @notice Unpause MaxBaner
    */
-  function unpause() external onlyRole(PAUSABLE_ROLE) whenPaused {
+  function unpause() external onlyOwner whenPaused {
     super._unpause();
   }
+
+  /**
+   * @notice Set mintable/burnable contract address
+   * @param _mintContract contract address to mint/burn tokens
+   */
+  function setMintContract(address _mintContract) external onlyOwner {
+    mintContract = _mintContract;
+  }
+
+  /**
+   * @notice Add address into the white list
+   * @param _addAddress address to add
+   */
+   function addWhiteList(address _addAddress) external onlyOwner {
+       require(!whiteList[_addAddress], "Already exists");
+       whiteList[_addAddress] = true;
+   }
+
+   /**
+   * @notice Remove address from the white list
+   * @param _removeAddress address to remove
+   */
+   function removeWhiteList(address _removeAddress) external onlyOwner {
+       require(whiteList[_removeAddress], "No exists");
+       whiteList[_removeAddress] = false;
+   }
+
+  /**
+   * @notice Set token transfer restriction
+   *    0 - No restriction,
+   *    1 - Only owner can send/receive tokens,
+   *    2 - Only addresses in the white list can receive tokens,
+   *    3 - The addressess in the white list can't receive tokens.
+   *    4+ - No restriction
+   * @param _transferControl transferring restriction
+   */
+    function setTransferCondtion(uint256 _transferControl) external onlyOwner {
+        transferControl = _transferControl;
+    }
+    
+    /**
+     * @notice Determine if token transfer is approved
+     * @param _from sender address
+     * @param _to beneficiary address
+     * @param _amount token amount
+     * @return (bool) approved or not
+     */
+     function _transferApprover(address _from, address _to, uint256 _amount) internal returns (bool) {
+        if (transferControl == 1) {
+            if (_from != super.owner() && _to != super.owner()) {
+                emit TransferFailed(_from, _to, _amount, transferControl);
+                return false;
+            }
+        } else if (transferControl == 2) {
+            if (!whiteList[_to]) {
+                emit TransferFailed(_from, _to, _amount, transferControl);
+                return false;
+            }
+        } else if (transferControl == 3) {
+            if (whiteList[_to]) {
+                emit TransferFailed(_from, _to, _amount, transferControl);
+                return false;
+            }
+        }
+
+        return true;
+     }
+
+  /**
+   * @notice Hook that is called before any transfer of Tokens
+   * @param from sender address
+   * @param to beneficiary address
+   * @param amount token amount
+   */
+    function _beforeTokenTransfer(address from, address to, uint256 amount ) internal override {
+        require(_transferApprover(from, to, amount));
+    }
+
 }
