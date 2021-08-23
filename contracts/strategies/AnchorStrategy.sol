58b04c2b7163f5fe8aadc84990e1e78c3725e665
vhromek <vojtar1@gmail.com>
2021-08-23 16:41:31 +0200

    Anchor strategy updates

--- contracts/strategies/AnchorStrategy.sol
+++ contracts/strategies/AnchorStrategy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
@@ -17,7 +17,7 @@ import "../interfaces/IAnchorExchangeRateFeeder.sol";
  * @author Maxos
  */
 contract AnchorStrategy is IStrategyBase, IStrategyAssetValue, ReentrancyGuardUpgradeable {
-  using SafeERC20 for IERC20;
+  using SafeERC20 for IERC20Upgradeable;
 
   /*** Events ***/
   event InvestAnchorStrategy(uint256 amount);
@@ -35,7 +35,7 @@ contract AnchorStrategy is IStrategyBase, IStrategyAssetValue, ReentrancyGuardUp
   IAnchorExchangeRateFeeder public constant ANCHOR_EXCHANGERATEFEEDER = IAnchorExchangeRateFeeder(0xd7c4f5903De8A256a1f535AC71CeCe5750d5197a);
 
   // USDC token interacted with the strategy
-  IERC20 public constant USDC_TOKEN = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+  IERC20Upgradeable public constant USDC_TOKEN = IERC20Upgradeable(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
   // Decimal number for total shares
   uint256 public constant DECIMALS = 2;
@@ -80,12 +80,14 @@ contract AnchorStrategy is IStrategyBase, IStrategyAssetValue, ReentrancyGuardUp
    */
   function redeem(address _beneficiary, uint256 _amount) external override nonReentrant onlyBanker {
 
+    ANCHOR_CONVERSIONPOOL.redeem(_amount);
+    totalShares += _amount;
 
     emit RedeemAnchorStrategy(_beneficiary, _amount);
   }
 
   /**
-   * @notice Returns asset value of the strategy form ANCHOR_EXCHANGERATEFEEDER contract
+   * @notice Returns asset value of the strategy from ANCHOR_EXCHANGERATEFEEDER contract
    * @return (uint256) asset value of the strategy in USD
    */
   function strategyAssetValue() external view override returns (uint256) {
