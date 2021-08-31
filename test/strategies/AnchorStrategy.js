b489b48e16078a77b5750ce3735497567ebabdfd
vhromek <vojtar1@gmail.com>
2021-08-31 13:33:21 +0200

    Anchor strategy tests

--- test/strategies/AnchorStrategy.js
+++ test/strategies/AnchorStrategy.js
@@ -6,13 +6,14 @@ const { parseUnits, parseEther } = require("@ethersproject/units");
 const { swapETHForExactTokens } = require("../common/UniswapV2Router");
 chai.use(solidity);
 
-const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const USDC_ADDRESS = "0xE015FD30cCe08Bc10344D934bdb2292B1eC4BBBD";
 const anchorUSDC_ADDRESS = "0x92E68C8C24a0267fa962470618d2ffd21f9b6a95";
+const exchangeRateFeeder_ADDRESS = "0x79E0d9bD65196Ead00EE75aB78733B8489E8C1fA";
 
 const TEST_USDC_AMOUNT = parseUnits("1000", 6);
 
 describe("AnchorStrategy", function () {
-  let usdcToken, anchorStrategy, anchorUSDCVault, addressManager;
+  let usdcToken, anchorStrategy, anchorUSDCVault, addressManager, exchangeRateFeeder;
 
   before(async function () {
     [deployer, manager, banker] = await ethers.getSigners();
@@ -32,9 +33,10 @@ describe("AnchorStrategy", function () {
     // Get IAnchorConversionPool and USDC
     anchorUSDCVault = await ethers.getContractAt("IAnchorConversionPool", anchorUSDC_ADDRESS, deployer);
     usdcToken = await ethers.getContractAt("IERC20", USDC_ADDRESS, deployer);
+    exchangeRateFeeder = await ethers.getContractAt("IAnchorExchangeRateFeeder", exchangeRateFeeder_ADDRESS, deployer);
 
     // Swap ETH for USDC   
-    await swapETHForExactTokens(parseEther("10"), parseUnits("10000", 6), USDC_ADDRESS, anchorStrategy.address, deployer);
+    //await swapETHForExactTokens(parseEther("10"), parseUnits("10000", 6), USDC_ADDRESS, anchorStrategy.address, deployer);
   });
 
   describe("Initialize", function () {
@@ -55,7 +57,55 @@ describe("AnchorStrategy", function () {
     it("Should not invest with the amount greater than than strategy balance", async function () {
       await expect(anchorStrategy.connect(banker).invest(parseUnits("20000", 6))).to.revertedWith("Invalid amount");
     });
+  });
 
+  describe("Redeem", function () {
+    it("Should not redeem by non banker", async function () {
+      await expect(anchorStrategy.redeem(anchorStrategy.address, 1)).to.revertedWith("No banker");
+    });
 
+    it("Should not redeem with zero amount", async function () {
+      await expect(anchorStrategy.connect(banker).redeem(anchorStrategy.address, 0)).to.revertedWith("Invalid amount");
+    });
+
+    it("Should not redeem with the amount greater than total shares", async function () {
+      const shares = await anchorStrategy.totalShares();
+      var exchangeRate = await exchangeRateFeeder.exchangeRateOf(USDC_ADDRESS, false);
+
+      const exceedAmount = shares
+        .mul(exchangeRate)
+        .div(10 ** 6)
+        .add(parseEther("10", 6));
+      await expect(anchorStrategy.connect(banker).redeem(anchorStrategy.address, exceedAmount)).to.revertedWith(
+        "Invalid amount",
+      );
+    });
+
+    it("Should redeem by banker", async function () {
+      const shares = await anchorStrategy.totalShares();
+      var exchangeRate = await exchangeRateFeeder.exchangeRateOf(USDC_ADDRESS, false);
+      const usdcAmount = shares.mul(exchangeRate).div(10 ** 6);
+
+      const before = await usdcToken.balanceOf(anchorStrategy.address);
+      await anchorStrategy.connect(banker).redeem(anchorStrategy.address, usdcAmount);
+      const after = await usdcToken.balanceOf(anchorStrategy.address);
+
+      expect(TEST_USDC_AMOUNT.sub(after.sub(before)).lte(10)).to.eq(true);
+    });
+  });
+
+  describe("Anchor Contracts", function () {
+    it("Deposit", async function () {
+      await anchorUSDCVault.connect(banker).deposit(parseUnits("20000", 6));
+    });
+
+    it("Redeem", async function () {
+      await anchorUSDCVault.connect(banker).redeem(parseUnits("20000", 6))
+    });
+
+    it("Exchange Rate", async function () {
+      var exchangeRate = await exchangeRateFeeder.exchangeRateOf(USDC_ADDRESS, false);
+      console.log("Exchange rate", exchangeRate);
+    });
   });
 });
